88105817996b8b7c86653331a9d029c617f280d3
Pallavi-A-12 <pallavibhavani1@gmail.com>
2026-08-09 20:08:19 +0530

    created responsive navbar

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("Landing page");
+console.log("Navigation bar");
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+})
